7e1f721245ec7e9bbb2f9426c743bb48629f817c
koni24 <konisha.24.kar@gmail.com>
2023-06-07 09:27:25 +0530

    auth modal

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import Sidebar from '@/components/Sidebar'
+import './globals.css'
+import { Figtree } from 'next/font/google'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Music App',
+  description: 'Listen to music!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider />
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
+      </body>
+    </html>
+  );
+}
